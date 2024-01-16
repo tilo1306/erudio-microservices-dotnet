52345aab25a0d42c3a38fb6bbfdde4ca68ee977f
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-16 18:04:54 -0300

    Ajust Layout

---
+++ S10_ErudioMicroservices.NET6-DefiningTheLayoutAndConsumingProductsMicroservice/GeekShooping/GeekShopping.Web/Program.cs
@@ -0,0 +1,30 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+    );
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ S10_ErudioMicroservices.NET6-DefiningTheLayoutAndConsumingProductsMicroservice/GeekShooping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _cliente;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient cliente)
+        {
+            _cliente = cliente ?? throw new ArgumentNullException(nameof(cliente));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _cliente.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductsById(long id)
+        {
+            var response = await _cliente.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+        public async Task<ProductModel> CreateProduct(ProductModel model)
+        {
+            var response = await _cliente.PostAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else
+                throw new Exception("Something went wrong when calling API");
+        }
+        public async Task<ProductModel> UpdateProduct(ProductModel model)
+        {
+            var response = await _cliente.PutAsJson(BasePath, model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+            else
+                throw new Exception("Something went wrong when calling API");
+        }
+
+        public async Task<bool> DeleteProductById(long id)
+        {
+            var response = await _cliente.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+            else
+                throw new Exception("Something went wrong when calling API");
+        }
+    }
+}
