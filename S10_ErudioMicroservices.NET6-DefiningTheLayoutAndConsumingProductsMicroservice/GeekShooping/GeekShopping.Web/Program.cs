c7ae66dc4e13fcd129720d67e53e92823d37adc1
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-17 17:17:47 -0300

    Ajust Layout

--- S10_ErudioMicroservices.NET6-DefiningTheLayoutAndConsumingProductsMicroservice/GeekShooping/GeekShopping.Web/Program.cs
+++ S10_ErudioMicroservices.NET6-DefiningTheLayoutAndConsumingProductsMicroservice/GeekShooping/GeekShopping.Web/Program.cs
@@ -3,14 +3,14 @@ using GeekShopping.Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+    );
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-builder.Services.AddHttpClient<IProductService, ProductService>(
-    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
-    );
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
