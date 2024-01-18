24ad55d051a5ff1a2cf323d7f1f316315efb7f03
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-18 13:11:15 -0300

    add DbContext in Identity Server

---
+++ S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShooping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeekShooping.IdentityServer.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
