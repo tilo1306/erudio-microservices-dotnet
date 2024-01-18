0f1164ef42b65220c3e3f15b3a3f80bfa3e67634
Adriana Martins <adrianam754@gmail.com>
2024-01-18 19:25:28 -0300

    Enable HTTPS and config port

--- S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShooping.IdentityServer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GeekShooping.IdentityServer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
