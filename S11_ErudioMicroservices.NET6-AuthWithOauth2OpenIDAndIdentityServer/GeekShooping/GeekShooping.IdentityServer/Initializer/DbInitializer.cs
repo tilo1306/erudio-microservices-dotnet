dbb8041658341507eb0627e44c01f1ce7d171dbf
Adriana Martins <adrianam754@gmail.com>
2024-01-19 18:31:53 -0300

    create class responsivel for start Db

---
+++ S11_ErudioMicroservices.NET6-AuthWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShooping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using GeekShooping.IdentityServer.Configuration;
+using GeekShooping.IdentityServer.Model;
+using GeekShooping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace GeekShooping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly MySQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<ApplicationUser> _role;
+
+        public DbInitializer(MySQLContext context, UserManager<ApplicationUser> user, RoleManager<ApplicationUser> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null) return;
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Client)).GetAwaiter().GetResult();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ S11_ErudioMicroservices.NET6-AuthWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShooping.IdentityServer/Initializer/IDbInitializer.cs
@@ -0,0 +1,7 @@
+﻿namespace GeekShooping.IdentityServer.Initializer
+{
+    public interface IDbInitializer
+    {
+        public void Initialize();
+    }
+}
